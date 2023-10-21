6b20598e52fc59ef0589a4c08bdf26a3bdb510ec
kypanz <kypanz@gmail.com>
2023-10-21 15:10:29 -0300

    Adding the register user function

--- contracts/PrivateChat.sol
+++ contracts/PrivateChat.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.0;
 contract PrivateChat {
 
     struct User {
+        address addr;
         string pub;
     }
 
@@ -15,8 +16,18 @@ contract PrivateChat {
         string[] messages;
     }
 
+    mapping(address => User) users;
     mapping(uint256 => Chat) chats;
 
     event MessageSended(uint256 _chatId, address _user, string _message);
+
+    uint256 chatId = 0;
+
+    function register(string memory _publicKey) public {
+        users[msg.sender] = User({
+            addr : msg.sender,
+            pub : _publicKey
+        });
+    }
 
 }
